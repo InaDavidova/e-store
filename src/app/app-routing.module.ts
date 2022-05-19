3bca71b3056a684c5c4b8df41c3c444f97c38330
Gabriel Stanev <47984623+Shroom88@users.noreply.github.com>
2022-05-20 01:10:51 +0300

    TODO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home'
-import { AboutComponent } from './components/about'
 
-const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home'
-import { AboutComponent } from './components/about'
+import { AboutComponent } from './components/about';
+import { CartComponent } from './components/cart-page/cart/cart.component'
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, AboutComponent
+    AppComponent, HomeComponent, AboutComponent, CartComponent
   ],
   imports: [
     BrowserModule,
