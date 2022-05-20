1fcf64b2c9b23042ed1da004120cbbd63becaad5
pkostov <pkostov@2create.studio>
2022-05-20 23:05:21 +0300

    adding login logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home'
 import { AboutComponent } from './components/about'
 import { DetailsComponent } from './components/details';
-import { LoginComponent } from './components/login';
-import { ProfileComponent } from './components/profile';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import {SignupComponent} from './signup/signup.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'details/:id', component: DetailsComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'signup', component: SignupComponent},
   { path: 'profile', component: ProfileComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home'
-import { AboutComponent } from './components/about'
+import { AboutComponent } from './components/about';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, AboutComponent
+    AppComponent, HomeComponent, AboutComponent, LoginComponent, SignupComponent, ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
