640ea5fa6894abd361100d439fcdaf831c495c15
Gabriel Stanev <47984623+Shroom88@users.noreply.github.com>
2022-05-21 14:27:27 +0300

    cart components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home'
 import { AboutComponent } from './components/about';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -22,6 +23,18 @@ import { CartComponent } from './components/cart-page/cart/cart.component'
   declarations: [
     AppComponent, HomeComponent, AboutComponent, CartComponent
 >>>>>>> 3bca71b (TODO)
+=======
+import { CartComponent } from './components/cart-page/cart/cart.component';
+import { ProductComponent } from './components/cart-page/product/product.component';
+import { ProductItemComponent } from './components/cart-page/product-item/product-item.component';
+import { CartProductComponent } from './components/cart-page/cart-product/cart-product.component';
+import { CartProductItemComponent } from './components/cart-page/cart-product-item/cart-product-item.component';
+import { CartCheckoutComponent } from './components/cart-page/cart-checkout/cart-checkout.component'
+
+@NgModule({
+  declarations: [
+    AppComponent, HomeComponent, AboutComponent, CartComponent, ProductComponent, ProductItemComponent, CartProductComponent, CartProductItemComponent, CartCheckoutComponent
+>>>>>>> c3dde74 (cart components generated)
   ],
   imports: [
     BrowserModule,
