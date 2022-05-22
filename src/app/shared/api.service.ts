017b628e3b9fe963937a66a02ca270ee7e61c58d
pkostov <pkostov@2create.studio>
2022-05-22 13:16:48 +0300

    unfinished update feature

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+  getProfile(){
+    return this.http.get<any>('http://localhost:3000/users')
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  updateProfile(data :any, id:number){
+    return this.http.put<any>('http://localhost:3000/users'+id, data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+}
