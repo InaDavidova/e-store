e6b81e177d7f555bdfaea51c0569e23411229e87
Ina Davidova <ina.davidovaa@gmail.com>
2022-06-10 00:44:30 +0300

    Cleaning comments from the code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChange } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TProduct } from 'src/app/shared/api.service';
 import { CartService } from 'src/app/shared/cart.service';
--- src/app/components/home-page/home/home.component.ts
+++ src/app/components/home-page/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService, TList, TProduct } from 'src/app/shared/api.service';
+import { ApiService, TList } from 'src/app/shared/api.service';
 
 @Component({
   selector: 'app-home',
